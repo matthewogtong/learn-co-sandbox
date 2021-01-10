72ef75f15a85abc037a9e97dd04fb9a1af1a50f9
Matthew Ogtong <ogtongm@gmail.com>
2021-01-10 16:47:44 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,39 @@
+# def first_loop_over_an_array(array)
+  
+#   counter = 0
+  
+#   while array[counter] do
+#     puts array[counter]
+#     counter += 1
+#   end
+# end
+
+# first_loop_over_an_array(['lets', 'see', 'if this works now'])
+
+# unsorted_array = ['peanut', 'ham', 'banana']
+
+# unsorted_array.sort!
+
+# puts unsorted_array
+
+# dog_one = {
+#   :name => "Luca",
+#   :breed => "German Shepherd"
+# }
+
+# dog_two = {
+#   :name => "Lola",
+#   :breed => "Giant Schnauzer"
+# }
+
+# puts dog_one[:name]
+
+# spice_rack = [
+#   ["Mace", "Ginger", "Marojam"],          
+#   ["Paprika", "Fajita Mix", "Coriander"], 
+#   ["Parsley", "Sage", "Rosemary"]         
+# ]
+
+# spice_rack[1][100] = "Poodle Dinner"
+
+# puts spice_rack
